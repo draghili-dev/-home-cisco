2c486d101c81f9bba014f3afdcc20d9f5fb19d24
Scott Stephenson <Scott4564@gmail.com>
2021-05-11 16:26:45 -0500

    Add files via upload

--- run-first.sh
+++ run-first.sh
@@ -1,5 +1,14 @@
 cat /etc/os-release > stats.txt
+pwd >> stats.txt
+who >> stats.txt
+echo "" >> stats.txt
+echo "Required Programs" >> stats.txt
 git --version >> stats.txt
+echo "" >> stats.txt
 docker --version >> stats.txt
+echo "" >> stats.txt
 ansible --version >>  stats.txt
-
+echo "" >> stats.txt
+echo "VS Code" >> stats.txt
+code --version >> stats.txt
+code --list-extensions >> stats.txt
